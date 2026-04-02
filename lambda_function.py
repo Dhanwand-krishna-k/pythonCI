1ada5def5aedb145155fde6b947bd6f9e56fb12b
Dhanwand-krishna-k <67233966+Dhanwand-krishna-k@users.noreply.github.com>
2026-04-02 16:53:05 +0530

    Update message to version 9 in lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
         
         "statusCode": 200,
         "body": {
-            "message":"version 8",
+            "message":"version 9",
             "type":"pipeline"
         },
         
